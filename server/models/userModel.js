00b06a097e22a52f52638b0800b707f75122fe66
Hasan Zahid <guszahidha@student.gu.se>
2025-11-15 18:05:28 +0100

    #3 add user schema for mogoose in UserModel.js

---
+++ server/models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        passwordHash: {
+            type: String,
+            required: true,
+        },
+        institution: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        role: {
+            type: String,
+            enum: ['Student', 'Alumni', 'Teacher', 'TA', 'other'],
+            required: true,
+        },
+        otherRoleName: {
+            type: String,
+            required: true,
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true}
+);
+
+module.exports = User;
