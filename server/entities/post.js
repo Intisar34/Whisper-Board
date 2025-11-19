e4d2fcc72cabf217e1cdd481b5d449051798e828
Intisar Warfa <warfa@student.chalmers.se>
2025-11-19 13:27:38 +0100

    #4 Add GET routes for fetching all posts and post by ID

--- server/entities/post.js
+++ server/entities/post.js
@@ -1,18 +1,42 @@
-var express = require('express');
-var router = express.Router();
-var Post = require('../models/postsModel');
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/postsModel');
 
+//Create a post
 router.post('/api/posts', async function(req,res, next){ 
     console.log('Recieved POST request'); 
     try{
-        var post = new Post(req.body);
-        var savedPost = await post.save();
+        const post = new Post(req.body);
+        const savedPost = await post.save();
 
         res.status(201).json(savedPost) 
-
     } catch (err) {
+        next(err);
+    }
+}); 
+
+//Get all posts
+router.get('/api/posts', async function(req,res,next){
+    try{
+      const getPosts = await Post.find()      
+      res.status(200).json({ getPosts });
+    } catch(err) {
         next(err);
     }
 });
 
-module.exports = router;
+//Get specific Post
+router.get('/api/posts/:id', async function(req,res,next){
+    try{
+      const postID = req.params.id;// get id from URL
+      const uniquePost = await Post.findById(postID); //find the post by ID   
+      
+      if(!uniquePost){
+        return res.status(404).json({message: "'Post not found"})
+      }
+      res.status(200).json({ uniquePost });
+    } catch(err) {
+        next(err);
+    }
+});
+
