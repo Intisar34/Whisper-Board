fb41b86e10b2926281e7261f171b9fd9c05f05a0
Intisar Warfa <warfa@student.chalmers.se>
2025-11-19 20:44:35 +0100

    #4 Add routes for relationship between entities

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -9,15 +9,24 @@ const {
     updatePost,
     patchPost,
     deletePost,
+    createPostForUser,
+    createPostInForum,
+    deleteUserSpecificPost
+
 } = require("../controllers/postController");
 
-router.post("/", createPost);
-router.get("/", getAllPosts);
-router.delete("/", deleteAllPosts)
-router.get("/:id", getPost);
-router.put("/:id", updatePost);
-router.patch("/:id", patchPost);
-router.delete("/:id",deletePost);
+router.post("/posts", createPost);
+router.get("/posts", getAllPosts);
+router.delete("/posts", deleteAllPosts)
+router.get("/posts/:id", getPost);
+router.delete("/posts/:id",deletePost);
+router.put("/posts/:id", updatePost);
+router.patch("/posts/:id", patchPost);
 
+// Extra relationship routes
+
+router.post("/users/:username/posts", createPostForUser);
+router.post("/forums/:forumID/posts", createPostInForum);
+router.delete("/users/:username/posts/:post_id", deleteUserSpecificPost);
 
 module.exports = router;
