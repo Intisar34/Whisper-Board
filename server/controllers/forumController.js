49c6e639226e5a46c93d1a5b1098296ac4b86560
Anisa Hashi <anisahas@student.chalmers.se>
2025-11-23 23:58:55 +0100

    #6 Add relationship between forums and posts

--- server/controllers/forumController.js
+++ server/controllers/forumController.js
@@ -1,11 +1,26 @@
 
-const Forum = require("../models/forumModel");
+const Forum = require('../models/forumModel');
+const Post = require('../models/postModel');
+
+// Creates a forum
+exports.createForums = async (req, res, next) =>{
+    try {
+        const forums = new Forum(req.body);
+        const saveForum = await forums.save();
+
+        res.status(201).json(saveForum) 
+    } catch (err) {
+        next(err);
+
+    }
+}; 
 
 // Gets all forums
 exports.getForums = async (req, res, next) => {
-    try{
+    try {
         const forums = await Forum.find();
-        res.json({data:forums});
+
+        res.status(200).json({data:forums});
     } catch(err) {
         next(err);
     }
@@ -27,7 +42,7 @@ exports.getForumByID = async (req, res, next) => {
     }
 };
 
-// Updates forums name/description
+// Updates forums 
 exports.updateForum = async (req, res, next) => {
     try {
         const {id} = req.params;
@@ -37,7 +52,7 @@ exports.updateForum = async (req, res, next) => {
             return res.status(404).json({error: "Forum not found!"});
         } 
         
-        res.json({data:forum});
+        res.status(200).json({data:forum});
         } catch (err) {
             next (err);
         }
@@ -50,7 +65,7 @@ exports.deleteForumByID = async (req, res, next) => {
         const forum = await Forum.findByIdAndDelete(id);
 
         if(!forum) {
-            return res.status(404).json({error:"Forum not found!"});
+            return res.status(404).json({error: "Forum not found!"});
         } 
         
         else {
@@ -62,3 +77,49 @@ exports.deleteForumByID = async (req, res, next) => {
     }
 };
 
+// Updates a forums field
+exports.updateForumField = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const forum = await Forum.findByIdAndUpdate(id, {$set: req.body}, {new: true, runValidators: true});
+
+        if(!forum) {
+            return res.status(404).json({error: "Forum not found!"});
+
+        }
+        res.status(200).json({data:forum});
+
+      } catch (err) {
+          next (err);
+      }
+};
+
+
+// Get all posts inside a forum
+exports.getForumPosts = async (req, res, next) => {
+    try {
+        const {forum_id} = req.params;
+        const posts = await Post.find({forumID: forum_id})
+
+        res.status(200).json({data: posts});
+    } catch (err) {
+        next(err);
+    }
+};
+
+// Create posts inside a forum
+exports.createForumPosts = async (req, res, next) => {
+    try {
+        const {forum_id} = req.params;
+        const post = new Post({
+            title: req.body.title,
+            body: req.body.body,
+            forumID: forum_id
+        });
+
+        const savePost = await post.save();
+        res.status(201).json(savePost);
+    } catch (err) {
+        next (err);
+    }
+};
--- server/routes/forumRoutes.js
+++ server/routes/forumRoutes.js
@@ -4,15 +4,26 @@ const router = express.Router();
 
 // Imports forum controller functions
 const {
+    createForums,
     getForums,
     getForumByID,
     deleteForumByID,
-    updateForum
+    updateForum,
+    updateForumField,
+    getForumPosts,
+    createForumPosts
 } = require("../controllers/forumController");
 
+router.post("/", createForums)
 router.get("/", getForums);
 router.get("/:id", getForumByID);
 router.delete("/:id", deleteForumByID);
 router.put("/:id", updateForum);
+router.patch("/:id", updateForumField);
+
+// Realationship routes
+router.get("/forums/:forum_id/posts", getForumPosts)
+router.post("/forums/:forum_id/posts", createForumPosts)
+
 
 module.exports = router;
