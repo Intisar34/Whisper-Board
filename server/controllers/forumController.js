0f9f17efb4cb00be2879a9557313344df91aa4de
Anisa Hashi <anisahas@student.chalmers.se>
2025-11-24 02:58:39 +0100

    #6 Add HTTP status for error handling

--- server/controllers/forumController.js
+++ server/controllers/forumController.js
@@ -5,10 +5,15 @@ const Forum = require('../models/forumModel');
 // Creates a forum
 exports.createForums = async (req, res, next) =>{
     try {
+        
+        if (!req.body.name) {
+            return res.status(400).json({error: "Forum name is required"});
+        }
+
         const forums = new Forum(req.body);
         const saveForum = await forums.save();
-
         res.status(201).json(saveForum) 
+
     } catch (err) {
         next(err);
 
@@ -21,6 +26,7 @@ exports.getForums = async (req, res, next) => {
         const forums = await Forum.find();
 
         res.status(200).json({data:forums});
+
     } catch(err) {
         next(err);
     }
@@ -46,6 +52,11 @@ exports.getForumByID = async (req, res, next) => {
 exports.updateForum = async (req, res, next) => {
     try {
         const {id} = req.params;
+
+        if (!req.body.name) {
+            return res.status(400).json({error: "Forum name is required"});
+        }
+
         const forum = await Forum.findByIdAndUpdate(id, req.body, {new: true, runValidators: true});
 
         if (!forum) {
@@ -68,19 +79,22 @@ exports.deleteForumByID = async (req, res, next) => {
             return res.status(404).json({error: "Forum not found!"});
         } 
         
-        else {
-            res.status(204).send();
-        }
+         res.status(204).send();
 
         } catch (err) {
             next(err);
     }
 };
 
-// Updates a forums field
+// Updates forums field
 exports.updateForumField = async (req, res, next) => {
     try {
         const {id} = req.params;
+
+        if (req.body.name !== undefined && req.body.name === "") {
+            return res.status(400).json({error: "Forum name cannot be empty!"});
+        }
+
         const forum = await Forum.findByIdAndUpdate(id, {$set: req.body}, {new: true, runValidators: true});
 
         if(!forum) {
