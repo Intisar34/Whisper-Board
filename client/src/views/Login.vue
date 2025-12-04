46a34e002787aacc4b1edee2a5d74c5a7f9c1bf5
Anisa Hashi <anisahas@student.chalmers.se>
2025-12-04 20:54:20 +0100

    #11 Create login page template

---
+++ client/src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="loginpage">
+        <div class="background">
+
+            <h1 class="logo"> WhisperBoard </h1>
+
+            <div class="loginbox">
+                <h2> Log in </h2>
+
+                <div class=username>
+                    <p> Username </p>
+                        <input type = "string" v-model="username"/>
+                </div>
+
+                <div class=password>
+                    <p> Password </p>
+                        <input type = "password" v-model="password"/>
+                </div>
+
+                <button type = "submit"> Log in </button>
+                <button type = "submit"> Forgot password? </button>
+             </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
