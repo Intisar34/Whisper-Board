86c1f3f53d3d0f551f97f7e51633e7d065021acf
Intisar Warfa <warfa@student.chalmers.se>
2025-12-05 04:46:16 +0100

    #12 Add sign up box for data input

--- client/src/views/SignUP.vue
+++ client/src/views/SignUP.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="background-color">
     <h1 class="logo"> WhisperBoard </h1>
+    <div class="paragraph-box">
+          <p class="paragraph">
+            A trusted space for students and faculty to discuss courses,
+            university life, and careers without revealing their identity.
+          </p>
+      </div>
+
+      <div class="sign-up-box">
+          <h1 class="title">Sign up</h1>
+      </div>
 
   </div>
 </template>
