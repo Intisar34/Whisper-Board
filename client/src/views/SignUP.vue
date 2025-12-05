959e7f8567f70a565680a588f29d662b0fa891fd
Intisar Warfa <warfa@student.chalmers.se>
2025-12-05 04:43:01 +0100

    #12 Add background color and logo to the page

---
+++ client/src/views/SignUP.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="background-color">
+    <h1 class="logo"> WhisperBoard </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUP'
+}
+
+</script>
+
+<style src="../styles/sign-up.css"></style>
