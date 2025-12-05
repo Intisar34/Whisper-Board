afd1bac87fa0d8ce1b4071be7e31a2a0d6d21134
Intisar Warfa <warfa@student.chalmers.se>
2025-12-05 04:53:55 +0100

    #12 Add sign-up form structure with inputs and buttons

--- client/src/views/SignUP.vue
+++ client/src/views/SignUP.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="background-color">
     <h1 class="logo"> WhisperBoard </h1>
+
     <div class="paragraph-box">
           <p class="paragraph">
             A trusted space for students and faculty to discuss courses,
@@ -10,8 +11,31 @@
 
       <div class="sign-up-box">
           <h1 class="title">Sign up</h1>
-      </div>
+          <div class=full-name>
+            <p> Full Name</p>
+              <input type= "String" v-model="fullName"/>
+        </div>
 
+     <div class="password-row">
+        <div class=password>
+            <p> Password</p>
+              <input type= "password" v-model="password"/>
+        </div>
+
+        <div class=confirm-password>
+            <p> Confirm Password</p>
+              <input type= "password" v-model="password"/>
+        </div>
+     </div>
+
+    <div class=email-address>
+            <p> Email Address</p>
+              <input type= "String" v-model="emailAddress"/>
+    </div>
+        <button class="create-button" type="submit">Create Account</button>
+                --- Already have an account? ---
+        <button class="login-button" type="submit"> Log In</button>
+      </div>
   </div>
 </template>
 
