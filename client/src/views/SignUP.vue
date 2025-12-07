da029b1087eb0b380b51b8f0f5c8a3184ba2ed35
Intisar Warfa <warfa@student.chalmers.se>
2025-12-07 15:18:56 +0100

    #15 Refactor Sign up form structure and add proper labels

--- client/src/views/SignUP.vue
+++ client/src/views/SignUP.vue
@@ -3,39 +3,42 @@
     <h1 class="logo"> WhisperBoard </h1>
 
     <div class="paragraph-box">
-          <p class="paragraph">
-            A trusted space for students and faculty to discuss courses,
-            university life, and careers without revealing their identity.
-          </p>
-      </div>
-
-      <div class="sign-up-box">
-          <h1 class="title">Sign up</h1>
-          <div class=full-name>
-            <p> Full Name</p>
-              <input type= "String" v-model="fullName"/>
-        </div>
-
-     <div class="password-row">
-        <div class=password>
-            <p> Password</p>
-              <input type= "password" v-model="password"/>
-        </div>
-
-        <div class=confirm-password>
-            <p> Confirm Password</p>
-              <input type= "password" v-model="password"/>
-        </div>
-     </div>
-
-    <div class=email-address>
-            <p> Email Address</p>
-              <input type= "String" v-model="emailAddress"/>
+      <p class="paragraph">
+        A trusted space for students and faculty to discuss courses,
+        university life, and careers without revealing their identity.
+      </p>
     </div>
+
+    <div class="sign-up-box">
+      <h1 class="title">Sign up</h1>
+
+      <form>
+        <div class="full-name">
+          <label for="fullName">Full Name</label>
+          <input id="fullName" type="text" v-model="fullName">
+        </div>
+
+        <div class="email-address">
+          <label for="emailAddress">Email Address</label>
+          <input id="emailAddress" type="email" v-model="emailAddress">
+        </div>
+
+        <div class="password-row">
+          <div class="password">
+            <label for="password">Password</label>
+            <input id="password" type="password" v-model="password">
+          </div>
+
+          <div class="confirm-password">
+            <label for="confirmPassword">Confirm Password</label>
+            <input id="confirmPassword" type="password" v-model="confirmPassword">
+          </div>
+        </div>
         <button class="create-button" type="submit">Create Account</button>
-                --- Already have an account? ---
-        <button class="login-button" type="submit"> Log In</button>
-      </div>
+          <p>--- Already have an account? ---</p>
+        <button class="login-button" type="button">Log In</button>
+      </form>
+    </div>
   </div>
 </template>
 
