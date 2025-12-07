e392d9eb04eec1b83defc659aa392ee7dd1192b6
Intisar Warfa <warfa@student.chalmers.se>
2025-12-07 22:43:44 +0100

    #15 Rename variables to camelCase

--- client/src/views/SignUP.vue
+++ client/src/views/SignUP.vue
@@ -1,42 +1,42 @@
 <template>
-  <div class="background-color">
+  <div class="backgroundColor">
     <h1 class="logo"> WhisperBoard </h1>
 
-    <div class="paragraph-box">
+    <div class="paragraphBox">
       <p class="paragraph">
         A trusted space for students and faculty to discuss courses,
         university life, and careers without revealing their identity.
       </p>
     </div>
 
-    <div class="sign-up-box">
+    <div class="signUpBox">
       <h1 class="title">Sign up</h1>
 
       <form>
-        <div class="full-name">
+        <div class="fullName">
           <label for="fullName">Full Name</label>
           <input id="fullName" type="text" v-model="fullName">
         </div>
 
-        <div class="email-address">
-          <label for="emailAddress">Email Address</label>
-          <input id="emailAddress" type="email" v-model="emailAddress">
+        <div class="email">
+          <label for="email">Email Address</label>
+          <input id="email" type="email" v-model="email">
         </div>
 
-        <div class="password-row">
+        <div class="passwordRow">
           <div class="password">
             <label for="password">Password</label>
             <input id="password" type="password" v-model="password">
           </div>
 
-          <div class="confirm-password">
+          <div class="confirmPassword">
             <label for="confirmPassword">Confirm Password</label>
             <input id="confirmPassword" type="password" v-model="confirmPassword">
           </div>
         </div>
-        <button class="create-button" type="submit">Create Account</button>
+        <button class="createButton" type="submit">Create Account</button>
           <p>--- Already have an account? ---</p>
-        <button class="login-button" type="button">Log In</button>
+        <button class="loginButton" type="button">Log In</button>
       </form>
     </div>
   </div>
@@ -49,4 +49,4 @@ export default {
 
 </script>
 
-<style src="../styles/sign-up.css"></style>
+<style src="../styles/signUp.css"></style>
