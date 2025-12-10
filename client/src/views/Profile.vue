ceb2c10446e454ecfe5358ceab4cb654215ee35b
Intisar Warfa <warfa@student.chalmers.se>
2025-12-10 17:20:19 +0100

    #17 Add profile banner with username and role

---
+++ client/src/views/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="backgroundPage">
+    <div class="profileBanner">
+
+      <div class="profileHeader">
+        <div class="headerContent">
+               <BAvatar size="4em" src="/userIcon.png" />
+            <div class="profileInfo">
+                <h1 class="profileName">Username</h1>
+                <h2 class="profileRole">Student</h2>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'Profile'
+}
+</script>
+
+<style src="../styles/profile.css"></style>
