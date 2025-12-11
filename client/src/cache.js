af139755eccf906ec263a68d56e9f6d87d14b6ce
Anisa Hashi <anisahas@student.chalmers.se>
2025-12-11 22:22:13 +0100

    #19 Add comments to the cache file

--- client/src/cache.js
+++ client/src/cache.js
@@ -1,11 +1,14 @@
+// Stores user in the localStorage
 export function storeUser(username) {
   localStorage.setItem('user', JSON.stringify({ username }))
 };
 
+// Gets user from the localStorage
 export function getUser() {
   localStorage.getItem('user')
 };
 
+// Deletes user from the localStorage
 export function deleteUser() {
   localStorage.removeItem('user')
 };
