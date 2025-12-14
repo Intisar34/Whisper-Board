4986ef7a41be515cc03a19c27cec0d3d8def8187
Intisar Warfa <warfa@student.chalmers.se>
2025-12-14 23:59:03 +0100

    #21 Implement patch requests to update user-specific fields

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -4,22 +4,27 @@
 
       <div class="profileHeader">
         <div class="headerContent">
-              <BAvatar size="4em" src="/userIcon.png" />
+              <BAvatar size="4em" src="/userIcon.png" rounded="sm" class="align-self-start"/>
             <div class="profileInfo">
-                <h1 class="profileName">Username</h1>
-                <h2 class="profileRole">Student</h2>
+                <h1 class="profileName" >{{ form.username }}</h1>
+                <h2 class="profileRole"> {{ form.role }}</h2>
+                <h2 class="profileInstitution">{{ form.institution }}</h2>
+            </div>
+            <div class="emailData">
+                <h2 class="profileEmail">{{ form.email }}</h2>
             </div>
         </div>
-        <BButton @click="toggleModel = !toggleModel" class="editButton"> Edit Profile </BButton>
+
+        <BButton @click="toggleModel = true;" class="editButton"> Edit Profile </BButton>
             <BModal v-model="toggleModel" size="xl"  content-class="tallModal" title-class="title" title="User Details">
                 <BForm @submit.prevent="saveProfile" class="formBox">
 
-                 <BFormGroup id="input-group-1" label="Username" label-for="input-1">
-                    <BFormInput id="input-1" v-model="form.username" />
+                  <BFormGroup id="input-group-1" label="Username" label-for="input-1">
+                    <BFormInput id="input-1" v-model="form.username" readonly/>
                  </BFormGroup>
 
                  <BFormGroup id="input-group-2" label="University" label-for="input-2">
-                    <BFormInput id="input-2" v-model="form.university" />
+                    <BFormInput id="input-2" v-model="form.institution" />
                  </BFormGroup>
 
                  <BFormGroup id="input-group-3" label="Email Address" label-for="input-3">
@@ -31,9 +36,14 @@
                  </BFormGroup>
 
                  <BFormGroup id="input-group-5" label="Role" label-for="input-5">
-                    <BFormInput id="input-5" v-model="form.role" />
+                    <BFormSelect id="input-5" v-model="form.role" :options="roleOptions"/>
                  </BFormGroup>
+
+                 <div class="d-flex justify-content-end mt-4">
+                  <BButton type="submit" variant="primary"> Save changes</BButton>
+                 </div>
                 </BForm>
+
              <div class="buttonContainer">
                 <BButton class="deleteButton">Delete</BButton>
              </div>
@@ -86,21 +96,113 @@ export default {
       forums: [],
       comments: [],
       timeline: [],
+      form: {
+        username: '',
+        institution: '',
+        email: '',
+        role: '',
+        password: ''
+      },
+      roleOptions: [
+        { text: 'Student' },
+        { text: 'Teacher' },
+        { text: 'Alumni' },
+        { text: 'TA' }
+      ],
+
+      originalForm: {},
 
       activeTab: 'posts',
-
-      form: {
-        username: 'username',
-        password: 'passoword',
-        email: 'email',
-        role: 'student'
-      },
 
       user: getUser()
     }
   },
 
+  mounted() {
+    this.fetchUserDetail()
+  },
+
   methods: {
+
+    // Fetch user details
+    async fetchUserDetail() {
+      try {
+        const response = await Api.get(`/users/${this.user.username}`)
+        const user = response.data.data
+        console.log(user)
+
+        this.form.username = user.username
+        this.form.institution = user.institution
+        this.form.email = user.email
+        this.form.role = user.role
+        this.form.password = user.password
+
+        // Save a copy of the form
+        this.originalForm = {
+          email: user.email,
+          institution: user.institution,
+          role: user.role
+        }
+      } catch (err) {
+        console.error('Failed fetching user details: ', err)
+      }
+    },
+
+    // Update user email
+    async updateUserEmail() {
+      try {
+        const request = await Api.patch(`/users/${this.user.username}`, {
+          email: this.form.email
+        })
+        console.log(request.data.data)
+        this.fetchUserDetail()
+        this.toggleModel = false
+      } catch (err) {
+        console.error('Failed updating user email: ', err)
+      }
+    },
+
+    // Update user role
+    async updateUserRole() {
+      try {
+        const request = await Api.patch(`/users/${this.user.username}`, {
+          role: this.form.role
+        })
+        console.log(request.data.data)
+        this.fetchUserDetail()
+        this.toggleModel = false
+      } catch (err) {
+        console.error('Failed updating user role: ', err)
+      }
+    },
+
+    // Update user institution
+    async updateUserInstitution() {
+      try {
+        const request = await Api.patch(`/users/${this.user.username}`, {
+          institution: this.form.institution
+        })
+        console.log(request.data.data)
+        this.fetchUserDetail()
+        this.toggleModel = false
+      } catch (err) {
+        console.error('Failed updating user institution: ', err)
+      }
+    },
+
+    // Update user password
+    async updateUserPassword() {
+      try {
+        const request = await Api.patch(`/users/${this.user.username}`, {
+          passowrd: this.form.password
+        })
+        console.log(request.data.data)
+        this.fetchUserDetail()
+        this.toggleModel = false
+      } catch (err) {
+        console.error('Failed updating user password: ', err)
+      }
+    },
 
     // Fetch all posts the user has
     async fetchPostUser() {
@@ -160,9 +262,39 @@ export default {
       }
     },
 
-    saveProfile() {
-      console.log('Saved Changes:', this.form)
-      this.toggleModel = false
+    // Save any changes made
+    async saveProfile() {
+      try {
+        const updates = []
+
+        if (this.form.institution !== this.originalForm.institution) {
+          updates.push(this.updateUserInstitution())
+        }
+
+        if (this.form.email !== this.originalForm.email) {
+          updates.push(this.updateUserEmail())
+        }
+
+        if (this.form.role !== this.originalForm.role) {
+          updates.push(this.updateUserRole())
+        }
+
+        if (this.form.password !== this.originalForm.password) {
+          updates.push(this.updateUserPassword())
+        }
+
+        if (updates.length === 0) {
+          console.log('No changes made')
+          return
+        }
+
+        await Promise.all(updates)
+
+        this.fetchUserDetail()
+        this.toggleModel = false
+      } catch (err) {
+        console.error('Failed Saving the Profile:', err)
+      }
     },
 
     setTab(tab) {
