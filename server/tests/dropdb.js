3633850e623f139602e98e58d590e312ae547450
Hasan Zahid <guszahidha@student.gu.se>
2026-01-01 19:42:56 +0100

    #39 restore dropdb.js

--- server/tests/dropdb.js
+++ server/tests/dropdb.js
@@ -9,15 +9,14 @@ if (!mongoURI) {
 }
 
 // Drop database
-mongoose.connect(mongoURI).then(function () {
-    mongoose.connection.dropDatabase().then(function () {
-        console.log(`Dropped database: ${mongoURI}`);
-        process.exit(0);
-    });
-}).catch(function (err) {
+mongoose.connect(mongoURI).catch(function (err) {
     if (err) {
         console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
         console.error(err.stack);
         process.exit(1);
     }
+});
+mongoose.connection.dropDatabase().then(function () {
+    console.log(`Dropped database: ${mongoURI}`);
+    process.exit(0);
 });
